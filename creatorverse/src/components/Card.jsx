653272972dced6ff4403eaafbf52afab50c69222
huyvu38 <vu38@purdue.edu>
2026-05-17 00:07:04 -0400

    View content creator

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -1,21 +1,27 @@
 function Card({ creator }) {
     return (
-        <div>
-            <h2>{creator.name}</h2>
-
-            <p>{creator.description}</p>
-
-            <a href={creator.url} target="_blank">
-                Visit Channel
-            </a>
+        <div className="card">
 
             {creator.imageURL && (
                 <img
                     src={creator.imageURL}
                     alt={creator.name}
-                    width="200"
+                    width="300"
                 />
             )}
+
+            <h2>{creator.name}</h2>
+
+            <p>{creator.description}</p>
+
+            <a
+                href={creator.url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Visit Channel
+            </a>
+
         </div>
     )
 }
--- creatorverse/src/pages/ShowCreators.jsx
+++ creatorverse/src/pages/ShowCreators.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../client'
+import Card from '../components/Card'
+
 function ShowCreators() {
+
+    const [creators, setCreators] = useState([])
+
+    useEffect(() => {
+        async function fetchCreators() {
+
+            const { data: creators, error } = await supabase
+                .from('creators')
+                .select()
+
+            console.log("CREATORS:", creators)
+            console.log("ERROR:", error)
+
+            if (error) {
+                console.error(error)
+                return
+            }
+
+            setCreators(creators || [])
+        }
+
+        fetchCreators()
+    }, [])
+
     return (
         <div>
             <h1>Creatorverse</h1>
+
+            {creators.length === 0 ? (
+                <p>No creators found.</p>
+            ) : (
+                creators.map((creator) => (
+                    <Card
+                        key={creator.id}
+                        creator={creator}
+                    />
+                ))
+            )}
         </div>
     )
 }
